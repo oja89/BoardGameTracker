33549450522e2863ae48aca74195ec3af01018fb
oja89 <markus.oja@student.oulu.fi>
2023-02-01 15:52:18 +0200

    DB creation with relationships working

--- create_db.py
+++ create_db.py
@@ -8,6 +8,8 @@ ctx.push()
 db.create_all()
 print("Db created")
 
+### Basic resources
+
 # add a player
 p = Players(name='Nick')
 
@@ -15,6 +17,7 @@ db.session.add(p)
 db.session.commit()
 
 print(f"Added player {p.name} as {p}")
+
 
 # add a team
 t = Teams(name='Foxes')
@@ -24,38 +27,75 @@ db.session.commit()
 
 print(f"Added team {t.name} as {t}")
 
-# add a map
-m = Maps(name='dust')
 
+
+### Resources with relationships
+
+## Game, rules, maps
+
+#add a game
+g = Games(name='CS:GO')
+db.session.add(g)
+db.session.commit()
+print(f"Added game {g.name} as {g}")
+
+# add map for the game
+m = Maps(
+    name='dust', 
+    game_id=g.id
+    )
 db.session.add(m)
 db.session.commit()
+print(f"Added map {m.name} as {m} for game {g.name} {g}")
 
-print(f"Added map {m.name} as {m}")
-
-# add a ruleset
-r = Rulesets(name='competitive')
-
+# add ruleset for the game
+r = Rulesets(
+    name='competitive', 
+    game_id=g.id
+    )
 db.session.add(r)
 db.session.commit()
 
-print(f"Added ruleset {r.name} as {r}")
+print(f"Added ruleset {r.name} as {r} for game {g.name} {g}")
 
-# add a game
-g = Games(name='CS:GO')
 
-db.session.add(g)
-db.session.commit()
-
-print(f"Added game {g.name} as {g}")
-
-m1 = Matches(date="sorrythisisstring", turns=30)
+### Match
+# match info, use game, map, ruleset
+m1 = Matches(
+    date="sorrythisisstring", 
+    turns=30,
+    game_id=g.id,
+    map_id=m.id,
+    ruleset_id=r.id
+    )
 
 db.session.add(m1)
 db.session.commit()
 
-print(f"Added match on date {m1.date} as {m1}")
+print(f"Added match on date {m1.date} as {m1}, with {g}, {m}, {r}")
 
-pr = Player_results(points=23)
+
+
+# team results, use match, team
+tr = Team_results(
+    points=56,
+    order=2,
+    match_id=m1.id,
+    team_id=t.id
+    )
+
+db.session.add(tr)
+db.session.commit()
+
+print(f"Added team score {tr.points} as {tr} for match {m1}")
+
+# player results, use match, player, team
+pr = Player_results(
+    points=23,
+    match_id=m1.id,
+    team_id=t.id,
+    player_id=p.id
+    )
 
 db.session.add(pr)
 db.session.commit()
@@ -63,12 +103,19 @@ db.session.commit()
 print(f"Added player score {pr.points} as {pr}")
 
 
-tr = Team_results(points=56)
 
-db.session.add(tr)
+
+
+
+
 db.session.commit()
 
-print(f"Added team score {tr.points} as {tr}")
+
+
+
+
+
+
 
 # remove app context
 ctx.pop()
--- db.py
+++ db.py
@@ -1,11 +1,21 @@
 from flask import Flask, request, json
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.exc import IntegrityError
+from sqlalchemy.engine import Engine
+from sqlalchemy import event
+
+DATABASE = "sqlite:///maindata.db"
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///maindata.db"
+app.config["SQLALCHEMY_DATABASE_URI"] = DATABASE
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
+
+@event.listens_for(Engine, "connect")
+def set_sqlite_pragma(dbapi_connection, connection_record):
+    cursor = dbapi_connection.cursor()
+    cursor.execute("PRAGMA foreign_keys=ON")
+    cursor.close()
 
 class Players(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -19,13 +29,13 @@ class Maps(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(64), nullable=False)
     
-    #game_id = db.relationship("id", back_populates="games")
+    game_id = db.Column(db.Integer, db.ForeignKey("games.id", ondelete="SET NULL"))
     
 class Rulesets(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(64), nullable=False)
     
-    #game_id = db.relationship("id", back_populates="games")
+    game_id = db.Column(db.Integer, db.ForeignKey("games.id", ondelete="SET NULL"))
     
 
 class Games(db.Model):
@@ -38,14 +48,22 @@ class Matches(db.Model):
     date = db.Column(db.String(64), nullable=False)
     turns = db.Column(db.Integer, nullable=False)
     
-    #game_id = db.relationship("id", back_populates="games")
-    #ruleset_id = db.relationship("id", back_populates="rulesets")
-    #map_id = db.relationship("id", back_populates="maps")
+    game_id = db.Column(db.Integer, db.ForeignKey("games.id", ondelete="SET NULL"))
+    ruleset_id = db.Column(db.Integer, db.ForeignKey("rulesets.id", ondelete="SET NULL"))
+    map_id = db.Column(db.Integer, db.ForeignKey("maps.id", ondelete="SET NULL"))
 
 class Player_results(db.Model):
     row = db.Column(db.Integer, primary_key=True)
     points = db.Column(db.Integer, nullable=False)
     
+    match_id = db.Column(db.Integer, db.ForeignKey("matches.id", ondelete="SET NULL"))
+    player_id = db.Column(db.Integer, db.ForeignKey("players.id", ondelete="SET NULL"))
+    team_id = db.Column(db.Integer, db.ForeignKey("teams.id", ondelete="SET NULL"))
+    
 class Team_results(db.Model):
     row = db.Column(db.Integer, primary_key=True)
     points = db.Column(db.Integer, nullable=False)
+    order = db.Column(db.Integer, nullable=False)
+    
+    match_id = db.Column(db.Integer, db.ForeignKey("matches.id", ondelete="SET NULL"))
+    team_id = db.Column(db.Integer, db.ForeignKey("teams.id", ondelete="SET NULL"))
