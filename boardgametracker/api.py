4f96aa55395062303b2baae6f47a3444ffbc4f47
oja89 <markus.oja@student.oulu.fi>
2023-03-02 18:06:38 +0200

    Add bp for Players

--- boardgametracker/api.py
+++ boardgametracker/api.py
@@ -4,9 +4,15 @@
 from flask import Blueprint
 from flask_restful import Api
 
-from boardgametracker.resources.players import *
-
 api_bp = Blueprint("api", __name__, url_prefix="/api")
 api = Api(api_bp)
 
+# after api creation to avoid circular imports
+from boardgametracker.resources.players import PlayerCollection, PlayerItem
+
 api.add_resource(PlayerCollection, "/players/")
+api.add_resource(PlayerItem, "players/<player>/")
+
+@api_bp.route("/"):
+def index():
+    return ""
