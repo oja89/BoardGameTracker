53a8c8259db9774d65d7a2067e467a9eb38777bf
oja89 <markus.oja@student.oulu.fi>
2023-03-06 14:18:46 +0200

    Now 9.08 Pylint

--- boardgametracker/utils.py
+++ boardgametracker/utils.py
@@ -18,61 +18,115 @@ from boardgametracker.models import (
     )
 
 class PlayerConverter(BaseConverter):
+    '''
+    Converter for player URL
+    '''
     def to_python(self, value):
+        '''
+        URL to python
+        '''
         db_player = Player.query.filter_by(name=value).first()
         if db_player is None:
             raise NotFound
         return db_player
 
     def to_url(self, value):
+        '''
+        python to URL
+        '''
         return value.name
 
 class MatchConverter(BaseConverter):
+    '''
+    Converter for match URL
+    '''
     def to_python(self, value):
+        '''
+        URL to python
+        '''
         db_match = Match.query.filter_by(id=value).first()
         if db_match is None:
             raise NotFound
         return db_match
 
     def to_url(self, value):
+        '''
+        python to URL
+        '''
         return value.id
 
 class GameConverter(BaseConverter):
+    '''
+    Converter for game URL
+    '''
     def to_python(self, value):
+        '''
+        URL to python
+        '''
         db_game = Game.query.filter_by(name=value).first()
         if db_game is None:
             raise NotFound
         return db_game
 
     def to_url(self, value):
+        '''
+        python to URL
+        '''
         return value.name
 
 class TeamConverter(BaseConverter):
+    '''
+    Converter for team URL
+    '''
     def to_python(self, value):
+        '''
+        URL to python
+        '''
         db_team = Team.query.filter_by(name=value).first()
         if db_team is None:
             raise NotFound
         return db_team
 
     def to_url(self, value):
+        '''
+        python to URL
+        '''
         return value.name
 
 class RulesetConverter(BaseConverter):
+    '''
+    Converter for ruleset URL
+    '''
     def to_python(self, value):
+        '''
+        URL to python
+        '''
         db_ruleset = Ruleset.query.filter_by(id=value).first()
         if db_ruleset is None:
             raise NotFound
         return db_ruleset
 
     def to_url(self, value):
+        '''
+        python to URL
+        '''
         return value.id
 
 class MapConverter(BaseConverter):
+    '''
+    Converter for map url
+    '''
     def to_python(self, value):
+        '''
+        URL to python
+        '''
         db_map = Map.query.filter_by(id=value).first()
         if db_map is None:
             raise NotFound
         return db_map
 
     def to_url(self, value):
+        '''
+        python to URL
+        '''
         return value.id
