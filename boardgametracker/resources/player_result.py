63a73ce70329b58bc6d39c6f996b446c8d56204f
oja89 <markus.oja@student.oulu.fi>
2023-03-06 13:21:43 +0200

    Player result pylinted

--- boardgametracker/resources/player_result.py
+++ boardgametracker/resources/player_result.py
@@ -1,20 +1,19 @@
-# from sensorhub example
-# https://github.com/enkwolf/pwp-course-sensorhub-api-example/blob/master/sensorhub/resources/sensor.py
+'''
+Functions for team_result class objects
 
-import json
-from jsonschema import validate, ValidationError
-from flask import Response, request, url_for, abort
+from sensorhub example
+https://github.com/enkwolf/pwp-course-sensorhub-api-example/blob/master/sensorhub/resources/sensor.py
+'''
+
 from flask_restful import Resource
-from sqlalchemy.exc import IntegrityError
-from boardgametracker.models import Player, PlayerResult 
-from boardgametracker import db
-from boardgametracker.constants import *
-import datetime
-from datetime import datetime
-from werkzeug.exceptions import NotFound, Conflict, BadRequest, UnsupportedMediaType
+from boardgametracker.models import PlayerResult
 from boardgametracker import cache
 
+
 class PlayerResultCollection(Resource):
+    '''
+    Collection of player_results
+    '''
     @cache.cached(timeout=5)
     def get(self, match=None, player=None):
         '''
@@ -22,7 +21,7 @@ class PlayerResultCollection(Resource):
         If match is given, all for that match
         If player given, all for that player
         If both, that should be an item, so abort?
-        
+
         From exercise 2,
         https://lovelace.oulu.fi/ohjelmoitava-web/ohjelmoitava-web/implementing-rest-apis-with-flask/
         '''
@@ -30,28 +29,19 @@ class PlayerResultCollection(Resource):
 
         # do the query for all
         if player is None and match is None:
-            p_results = Player_result.query.all()
-            
+            p_results = PlayerResult.query.all()
+
         # do the query for given player
         elif match is None:
             player_id = player.serialize(long=True)["id"]
-            p_results = Player_result.query.filter_by(player_id=player_id)
-            
+            p_results = PlayerResult.query.filter_by(player_id=player_id)
         elif player is None:
             match_id = match.serialize(long=True)["id"]
-            p_results = Player_result.query.filter_by(match_id=match_id)
-        
-        else:
-        # should not come here
-        # both match and player given, that should find an item
-        # throw an error
-        # Todo: what error?
-            abort(400)
-            
+            p_results = PlayerResult.query.filter_by(match_id=match_id)
 
         for result in p_results:
             data_object.append(result.serialize(long=True))
-            
+
         response = data_object
-        
+
         return response, 200
