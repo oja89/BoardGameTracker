4117e533f95e0d9ac3662d656266701f431cd411
oja89 <markus.oja@student.oulu.fi>
2023-03-22 12:22:17 +0200

    Change to plurals

--- boardgametracker/api.py
+++ boardgametracker/api.py
@@ -18,17 +18,17 @@ from flask_restful import Api
 api_bp = Blueprint("api", __name__, url_prefix="/api")
 api = Api(api_bp)
 
-api.add_resource(PlayerCollection, "/player/")
+api.add_resource(PlayerCollection, "/players/")
 api.add_resource(PlayerItem, "/player/<player:player>/")
-api.add_resource(TeamCollection, "/team/")
+api.add_resource(TeamCollection, "/teams/")
 api.add_resource(TeamItem, "/team/<team:team>/")
-api.add_resource(GameCollection, "/game/")
+api.add_resource(GameCollection, "/games/")
 api.add_resource(GameItem, "/game/<game:game>/")
 
 # 2 routes for map
 api.add_resource(MapCollection,
-                 "/map/",
-                 "/game/<game:game>/map/"
+                 "/maps/",
+                 "/game/<game:game>/maps/"
                  )
 api.add_resource(MapItem,
                  "/map/<map_:map_>/",
@@ -37,23 +37,23 @@ api.add_resource(MapItem,
 
 # 2 routes for rulesets
 api.add_resource(RulesetCollection,
-                 "/ruleset/",
-                 "/game/<game:game>/ruleset/"
+                 "/rulesets/",
+                 "/game/<game:game>/rulesets/"
                  )
 api.add_resource(RulesetItem,
                  "/game/<game:game>/ruleset/<ruleset:ruleset>/",
                  "/ruleset/<ruleset:ruleset>/"
                  )
 
-api.add_resource(MatchCollection, "/match/")
+api.add_resource(MatchCollection, "/matches/")
 api.add_resource(MatchItem, "/match/<int:match>/")
 
 # results collections
 api.add_resource(PlayerResultCollection,
-                 "/player/<player:player>/result/",
-                 "/match/<int:match>/playerresult/"
+                 "/player/<player:player>/results/",
+                 "/match/<int:match>/playerresults/"
                  )
 api.add_resource(TeamResultCollection,
-                 "/team/<team:team>/result/",
-                 "/match/<int:match>/teamresult/"
+                 "/team/<team:team>/results/",
+                 "/match/<int:match>/teamresults/"
                  )
